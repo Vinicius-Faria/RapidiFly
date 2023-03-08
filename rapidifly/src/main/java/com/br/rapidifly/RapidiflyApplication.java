a763f3a4032d86a4bbc4bb0575b017209bc04d4b
Vinicius Augusto <viniciguto1@gmail.com>
2023-03-08 10:39:49 -0300

    Adcionando back - end raplidifly

---
+++ rapidifly/src/main/java/com/br/rapidifly/RapidiflyApplication.java
@@ -0,0 +1,13 @@
+package com.br.rapidifly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidiflyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RapidiflyApplication.class, args);
+    }
+
+}
